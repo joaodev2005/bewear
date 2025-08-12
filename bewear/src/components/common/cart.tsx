51f9c6615d66407485212279f9b99d4c8a9239dd
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-08-12 19:21:34 -0300

    feat: add cart item

--- bewear/src/components/common/cart.tsx
+++ bewear/src/components/common/cart.tsx
@@ -2,19 +2,19 @@
 
 import { useQuery } from '@tanstack/react-query'
 import { ShoppingBasketIcon } from 'lucide-react'
-import Image from 'next/image'
 import React from 'react'
 
 import { getCart } from '@/actions/get-cart'
 
 import { Button } from '../ui/button'
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet'
+import CartItem from './cart-item'
 
 const Cart = () => {
     const { data: cart, isPending: cartIsLoading } = useQuery({
         queryKey: ['cart'],
         queryFn: () => getCart(),
-        
+
     })
     return (
         <Sheet>
@@ -27,15 +27,18 @@ const Cart = () => {
                 <SheetHeader>
                     <SheetTitle>Carrinho</SheetTitle>
                 </SheetHeader>
-                <div>
+                <div className='space-y-4 px-5'>
                     {cartIsLoading && <p>Carregando...</p>}
                     {cart?.items.map((item) => (
-                        <div key={item.id}>
-                            <Image src={item.productVariant.imageUrl} alt={item.productVariant.name} width={100} height={100} />
-                            <div>
-                                <h3>{item.productVariant.name}</h3>
-                            </div>
-                        </div>
+                        <CartItem
+                            key={item.id} 
+                            id={item.id}
+                            productName={item.productVariant.product.name}
+                            productVariantName={item.productVariant.name}
+                            productVariantImageUrl={item.productVariant.imageUrl}
+                            productVariantPriceInCents={item.productVariant.priceInCents}
+                            quantity={item.quantity}
+                        />
                     ))}
                 </div>
             </SheetContent>
