2184eb1ef3a707aec6999f78210869f223b1a043
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-08-18 18:29:38 -0300

    fix: reorder exports and add missing React imports in UI components

--- bewear/next.config.ts
+++ bewear/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   images: {
     domains: ['fsc-projects-static.s3.us-east-1.amazonaws.com'],
   },
